78ce5daec381143a531cd41888340194fed16388
Ganesh Patil <ganeshpatil0101@gmail.com>
2022-07-27 20:27:52 +0530

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './pages/Layout';
-import Album from './pages/Album';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import AddProduct from './pages/AddProduct';
@@ -17,7 +16,6 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="details/:productId" element={<Details />} />
-            <Route path="album" element={<Album />} />
             <Route path="addproduct" element={<AddProduct />} />
           </Route>
         </Routes>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import {Link, Outlet} from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import NavBar from '../components/navbar';
 
 import { Container } from '@mui/system';
@@ -9,21 +9,10 @@ export default function Layout() {
         <>
         <NavBar />
         <Container sx={{ py: 8 }} maxWidth="md">
-            
-            
-            
-                <div>
-                {/* <nav>
-                    <Link to="/">Home</Link> |{" "}
-                    <Link to="details">details</Link> |{" "}
-                    <Link to="addproduct">addProduct</Link> |{" "}
-                    <Link to="album">album</Link> |{" "}
-                </nav>
-                <br /> */}
-                <Outlet />
-
-                </div>
-            </Container>
+            <div>
+            <Outlet />
+            </div>
+        </Container>
         </>
     )
 }
